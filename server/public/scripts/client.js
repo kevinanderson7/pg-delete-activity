8006e1193386ec22764e056fdfbdaca49b7ae3e2
Kevin Anderson <kmichael419@hotmail.com>
2020-07-09 16:04:20 -0500

    adding delete button and route and seems to be functioning correctly

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,50 +3,61 @@ $(document).ready(handleReady);
 function handleReady() {
   // Set up click listeners
   $('#submit').on('click', handleSubmit);
-
+  $('#shoeDisplay').on('click', '#deleteButton', clickDeleteShoe);
 
   // Get data
   getShoes();
 }
+function clickDeleteShoe() {
+  // console.log($(this).parent().parent().data('id'));
 
-function handleSubmit() { 
+  const id = $(this).parent().parent().data('id');
+  deleteShoe(id);
+}
+
+function handleSubmit() {
+  console.log('clicking submit');
+
   const name = $('#name').val();
   const size = $('#size').val();
   const cost = $('#cost').val();
 
   const objectToSend = {
-    name : name,
-    size : size,
-    cost : cost
-  }
+    name: name,
+    size: size,
+    cost: cost,
+  };
 
   $.ajax({
     method: 'POST',
     url: '/shoes',
-    data : objectToSend
-  }).then( function(response ) {
-    //database is updated, need to update DOM
-    getShoes();
-  }).catch(function(err){
-    console.log(err);
-    alert('Something went wrong in POST');
+    data: objectToSend,
   })
-
- }
+    .then(function (response) {
+      //database is updated, need to update DOM
+      getShoes();
+    })
+    .catch(function (err) {
+      console.log(err);
+      alert('Something went wrong in POST');
+    });
+}
 
 function getShoes() {
   $.ajax({
-      method: 'GET',
-      url: '/shoes'
-  }).then(function(response) {
+    method: 'GET',
+    url: '/shoes',
+  })
+    .then(function (response) {
       console.log('getShoes - response', response);
 
       //Append Shoes
       appendShoeTable(response);
-  }).catch(function(error){
+    })
+    .catch(function (error) {
       console.log('getShoes - error', error);
-      alert('something went wrong in GET')
-  })
+      alert('something went wrong in GET');
+    });
 }
 
 function appendShoeTable(shoes) {
@@ -61,8 +72,12 @@ function appendShoeTable(shoes) {
       <td>${shoe.name}</td>
       <td>${shoe.cost}</td>
       <td>${shoe.size}</td>
+      <td>
+        <button id="deleteButton">
+        DELETE
+      </button>
     </tr>
-    `)
+    `);
 
     // attach data to row, need for delete
     tableRow.data('id', shoe.id);
@@ -70,4 +85,18 @@ function appendShoeTable(shoes) {
     //append row to table
     $('#shoeDisplay').append(tableRow);
   }
+}
+
+function deleteShoe(shoeId) {
+  $.ajax({
+    type: 'DELETE',
+    url: `/shoes/${shoeId}`,
+  })
+    .then((response) => {
+      getShoes();
+    })
+    .catch((error) => {
+      console.log('error: ', error);
+      alert('Stuff broke!');
+    });
 }
--- server/routes/shoe.router.js
+++ server/routes/shoe.router.js
@@ -6,50 +6,72 @@ const router = express.Router();
 const pg = require('pg');
 const Pool = pg.Pool;
 const config = {
-    database: 'shoe_store', // name of database
-    host: 'localhost',
-    port: 5432,
-    max: 10, // max number of concurrent connections
-    idleTimeoutMillis: 10000 // attepmt to connect for 10 seconds
-}
+  database: 'shoe_store', // name of database
+  host: 'localhost',
+  port: 5432,
+  max: 10, // max number of concurrent connections
+  idleTimeoutMillis: 10000, // attepmt to connect for 10 seconds
+};
 
 const pool = new Pool(config);
 
 pool.on('connect', () => {
-    console.log('postgresql connected!!!');
+  console.log('postgresql connected!!!');
 });
 
 pool.on('error', (error) => {
-    console.log('Error connecting to db', error);
+  console.log('Error connecting to db', error);
 });
 
 // Express removed the '/shoes' when we do a app.use
 router.post('/', function (req, res) {
-    const shoeToAdd = req.body; // This the data we sent
-    console.log('In POST route - product:', shoeToAdd); // Has a name, size and cost
-    const query = 'INSERT INTO "shoes" ("name", "cost", "size") VALUES ($1, $2, $3);';
-    // $ with index (e.g. $1) will help improve the security of your db
-    // Avoids SQL injection -- see bobby drop table comic
-    pool.query(query, [shoeToAdd.name, shoeToAdd.cost, shoeToAdd.size]).then(() => {
-        res.sendStatus(201);
-    }).catch((error) => {
-        console.log('Error in POST', error);
-        res.sendStatus(500);
+  const shoeToAdd = req.body; // This the data we sent
+  console.log('In POST route - product:', shoeToAdd); // Has a name, size and cost
+  const query =
+    'INSERT INTO "shoes" ("name", "cost", "size") VALUES ($1, $2, $3);';
+  // $ with index (e.g. $1) will help improve the security of your db
+  // Avoids SQL injection -- see bobby drop table comic
+  pool
+    .query(query, [shoeToAdd.name, shoeToAdd.cost, shoeToAdd.size])
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log('Error in POST', error);
+      res.sendStatus(500);
     });
 });
 
 // http://localhost:5002/shoes will go here
 router.get('/', function (req, res) {
-    console.log('In GET route');
-    // The query we want to run
-    const query = 'SELECT * FROM "shoes";';
-    pool.query(query).then((results) => {
-        console.log(results); // This is an object
-        res.send(results.rows); // result.rows is an Array of shoes
-    }).catch((error) => {
-        console.log('Error making GET', error);
-        res.sendStatus(500);
+  console.log('In GET route');
+  // The query we want to run
+  const query = 'SELECT * FROM "shoes";';
+  pool
+    .query(query)
+    .then((results) => {
+      console.log(results); // This is an object
+      res.send(results.rows); // result.rows is an Array of shoes
+    })
+    .catch((error) => {
+      console.log('Error making GET', error);
+      res.sendStatus(500);
     });
 }); // END GET ROUTE
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  const queryText = `DELETE FROM "shoes" WHERE "id" = $1;`;
+
+  pool
+    .query(queryText, [id])
+    .then((dbResponse) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log('error on delete', error);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
